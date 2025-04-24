ec63e25c206e97f771b8b581ac766be4c4da391c
Jackson Case <126780741+NO-skcaj@users.noreply.github.com>
2025-04-24 11:55:04 -0400

    Update ChassisDrivePose.h

--- src/main/include/commands/ChassisDrivePose.h
+++ src/main/include/commands/ChassisDrivePose.h
@@ -17,20 +17,13 @@ namespace ChassisDrivePose
         return AutoBuilder::followPath(PathPlannerPath::fromPathFile(CommandName));
     }
 
-    inline frc2::CommandPtr ChassisDrivePose(std::function<frc::Pose2d()> currentPoseSupplier, // Current robot pose used as starting state 
-                                             frc::Pose2d targetPose)     // End goal state relative to the origin, blue alliance side
+    inline frc2::CommandPtr ChassisDrivePose(frc::Pose2d targetPose) // End goal state relative to the origin, blue alliance side
     {
-        frc::Pose2d currentPose = currentPoseSupplier();
+        return AutoBuilder::pathfindToPose(targetPose, Constraints);
+    }
 
-        // Create the path using the waypoints created above
-        // We make a shared pointer here since the path following commands require a shared pointer
-        auto path = std::make_shared<PathPlannerPath>(
-            PathPlannerPath::waypointsFromPoses(std::vector<frc::Pose2d>{currentPose, targetPose}), // The end pose for the path. You can also use a vector of waypoints
-            Constraints,                                       // The constraints for the path.
-            std::nullopt,
-            GoalEndState(0.0_mps, targetPose.Rotation())      // Goal end state. You can set a holonomic rotation here. If using a differential drivetrain, the rotation will have no effect.
-        );
-
-        return AutoBuilder::followPath(path);
+    inline frc2::CommandPtr ChassisDrivePoseFlipped(frc::Pose2d targetPose) // End goal state relative to the origin, blue alliance side then flipped to red
+    {
+        return AutoBuilder::pathfindToPose(targetPose, Constraints);
     }
 };
